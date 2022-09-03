0b8d7cfac2d60481bc74c547a26ea27813c92a0e
Shubham Bachhav <112254360+sbachhav1@users.noreply.github.com>
2022-09-03 12:09:44 +0530

    Create JsonFileFormat.py

---
+++ JsonFileFormat.py
@@ -0,0 +1,42 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import re
+import sys
+
+
+def read_nested_json(df):
+    column_list = []
+    for column_name in df.schema.names:
+        if isinstance(df.schema[column_name].dataType, ArrayType):
+            df = df.withColumn(column_name, explode(column_name))
+            column_list.append(column_name)
+        elif isinstance(df.schema[column_name].dataType, StructType):
+            for field in df.schema[column_name].dataType.fields:
+                column_list.append(col(column_name + "." + field.name).alias(column_name + "_" + field.name))
+        else:
+            column_list.append(column_name)
+    df = df.select(column_list)
+    return df;
+
+
+def trans(df):
+    read_nested_json_flag = True
+    while read_nested_json_flag:
+        df = read_nested_json(df);
+        read_nested_json_flag = False
+        for column_name in df.schema.names:
+            if isinstance(df.schema[column_name].dataType, ArrayType):
+                read_nested_json_flag = True
+            elif isinstance(df.schema[column_name].dataType, StructType):
+                read_nested_json_flag = True;
+    cols = [re.sub('[^a-zA-Z0-1]', "", c.lower()) for c in df.columns]
+    return df.toDF(*cols);
+
+a = "C:\\Bigdata\\datasets\\world_bank.json"
+#Creting DataFrame
+dff = spark.read.format('json').options(header='True',inferschema='True').load(a)
+#using UDF for this DataFrame
+ndff = trans(dff)
+ndff.show()
+ndff.printSchema()
